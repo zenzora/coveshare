f173434f7aca9f4959dbe34ac5fe5de0f45832df
Jake <jake>
2019-11-03 09:43:13 -0500

    server files

---
+++ cmd/serve.go
@@ -0,0 +1,32 @@
+/*
+Copyright © 2019 Jake Berkowsky
+
+*/
+package cmd
+
+import (
+	server "github.com/zenzora/coveshare/server"
+	"github.com/spf13/cobra"
+)
+
+// serveCmd represents the serve command
+var serveCmd = &cobra.Command{
+	Use:   "serve",
+	Run: func(cmd *cobra.Command, args []string) {
+		server.Serve()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(serveCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// serveCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// serveCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
