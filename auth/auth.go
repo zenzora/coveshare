52abb221a31885ae29c26781b54a1a71fa2e92ac
jake <jake>
2020-07-13 23:15:05 -0400

    jwt creation

--- auth/auth.go
+++ auth/auth.go
@@ -1,12 +1,32 @@
 package auth
 
-import "time"
+import (
+	"github.com/dgrijalva/jwt-go"
+	"github.com/spf13/viper"
+)
 
 // login via email - sends login link, with optional "redirect" to encrypted message
 
-// CreateLoginCode (from email)
-func CreateLoginCode(email string, expiration time.Time) {
+type authClaims struct {
+	email string
+	jwt.StandardClaims
+}
 
+// CreateLoginCode creates a JWT with an email and expiration
+func CreateLoginCode(email string) (string, error) {
+	claims := authClaims{
+		email,
+		jwt.StandardClaims{ // Not including an expiration for now
+			Issuer: "coveshare",
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	keyString := viper.GetString("key")
+	ss, err := token.SignedString(keyString)
+	if err != nil {
+		return ss, nil
+	}
+	return ss, err
 }
 
 // Validate login code
